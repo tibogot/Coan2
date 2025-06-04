fe44f6048126ea87ee4b5e490469b7949176364a
tibogot <tibogotbeatz@gmail.com>
2025-06-04 04:13:15 +0200

    k

--- src/pages/Home2.tsx
+++ src/pages/Home2.tsx
@@ -10,6 +10,7 @@ import Copy from "../components/Copy1";
 import GridComponent from "../components/GridComponent2";
 import HomeCard from "../components/CardsScroll11";
 import { useGSAP } from "@gsap/react";
+import Chart from "../components/Chart";
 // import CounterDark from "../components/CounterDark";
 // import CautionTicker from "../components/CautionTicker";
 
@@ -288,33 +289,9 @@ const Home = () => {
 
         {/* Chart Part*/}
 
-        <section className="font-NHD relative flex min-h-screen w-full flex-col overflow-hidden bg-black px-4 py-20 text-white md:px-10">
-          <div className="w-full md:flex">
-            <div className="left w-1/2">
-              <Copy>
-                <h2 className="w-3/4">
-                  Over 18 years grinding alongside founders with a chip.
-                </h2>
-              </Copy>
-            </div>
-            <div className="right w-1/2">
-              <Copy>
-                <p className="text-xl text-balance">
-                  MIMCO est un groupe d’investissement spécialisé dans
-                  l'immobilier value add paneuropéen. Le groupe structure et
-                  gère des véhicules d’investissement innovants au service d’une
-                  clientèle institutionnelle ainsi que de family offices et
-                  banques privées.
-                </p>
-              </Copy>
-            </div>
-          </div>
-          <div className="charts mt-10 flex w-full flex-1 items-end gap-10 bg-blue-500">
-            <div className="chart1 h-[300px] w-1/3 rounded-sm bg-orange-400"></div>
-            <div className="chart2 h-[500px] w-1/3 rounded-sm bg-orange-400"></div>
-            <div className="chart3 h-[400px] w-1/3 rounded-sm bg-orange-400"></div>
-          </div>
-        </section>
+        <Chart />
+
+        {/* <Counter /> */}
 
         {/* Team Section */}
 
